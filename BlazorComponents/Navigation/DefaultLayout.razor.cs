a6b5e083e797dbf8b50aa43a066a39eef0f58519
LM <lewis.macgillivray@sharp.eu>
2024-04-02 11:40:33 +0100

    Revert "Better banners using position sticky and some amendments to the default layout component"
    This reverts commit a330f74c8b5a6ac68b6d0f11609d58ec8327eb00.

--- BlazorComponents/Navigation/DefaultLayout.razor.cs
+++ BlazorComponents/Navigation/DefaultLayout.razor.cs
@@ -11,7 +11,6 @@ namespace BlazorComponents
 		[Parameter] public RenderFragment SearchBar { get; set; }
 		[Parameter] public RenderFragment NavMenu { get; set; }
 		[Parameter] public RenderFragment Content { get; set; }
-		[Parameter] public RenderFragment Banners { get; set; }
 		[Parameter] public bool FixNav { get; set; }
 
 		#endregion
